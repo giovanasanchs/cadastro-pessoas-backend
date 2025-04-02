7b2317d307a59b115cbe90a3585f36a19eefbf05
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-02 11:18:43 -0300

    :tada: criação do projeto

---
+++ src/test/java/com/giovana/cadastro_pessoas/CadastroPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovana.cadastro_pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
