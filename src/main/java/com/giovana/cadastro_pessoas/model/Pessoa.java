7fbb6af3a228a18dba874c57691f9c74407d236f
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-02 11:56:55 -0300

    :sparkles: feat: adicionando entidade Pessoa e enum e TipoPessoa na camada model

---
+++ src/main/java/com/giovana/cadastro_pessoas/model/Pessoa.java
@@ -0,0 +1,132 @@
+package com.giovana.cadastro_pessoas.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private TipoPessoa tipoPessoa; // ENUM para FÍSICA ou JURÍDICA
+
+    private String cpf;
+    private String cnpj;
+
+    @NotBlank
+    private String cep;
+
+    private String endereco;
+    private String numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    // Getters e Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipoPessoa getTipoPessoa() {
+        return tipoPessoa;
+    }
+
+    public void setTipoPessoa(TipoPessoa tipoPessoa) {
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/com/giovana/cadastro_pessoas/model/TipoPessoa.java
@@ -0,0 +1,5 @@
+package com.giovana.cadastro_pessoas.model;
+
+public enum TipoPessoa {
+    FISICA, JURIDICA
+}
