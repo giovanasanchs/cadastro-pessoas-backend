5f41c1e0ad4d7497cfe6fb64dec1ddc06e2eae10
Gih2139Sanches <giovanaclemente2139@gmail.com>
2025-04-04 13:22:19 -0300

    :wrench: chore: adicionando arquivo de cors para conxeão com o frontend

---
+++ src/main/java/com/giovana/cadastro_pessoas/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.giovana.cadastro_pessoas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todos os endpoints
+                        .allowedOrigins("http://localhost:5173") // Libera o frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
